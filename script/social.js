74b8038be1c18eb1d8a868e62fb19dc78ad26efb
maisha-maliha <officiallymaisha@gmail.com>
2020-09-23 19:30:41 +0600

    hello

---
+++ script/social.js
@@ -0,0 +1,3 @@
+const ul = document.querySelector("#follow .fol-link ul")
+
+ul.innerHTML = '<li><a href="https://www.facebook.com/maishamalihablog/"><img class="icon" src="images/icon/fb.png" alt=""></a></li><li><a href="https://www.instagram.com/maisha_maliha_/?hl=en"><img class="icon" src="images/icon/insta.png" alt=""></a></li><li><a href="https://twitter.com/Maisha_Maliha_"><img class="icon" src="images/icon/twt.png" alt=""></a></li><li><a href="https://www.youtube.com/channel/UCi4-CipsLOgdqVsmIY0lXIA"><img class="icon" src="images/icon/you.png" alt=""></a></li><li><a href="https://linkedin.com/in/maishaofficially"><img class="icon" src="images/icon/link.png" alt=""></a></li><li><a href="https://github.com/maisha-maliha"><img class="icon" src="images/icon/git.png" alt=""></a></li>'
---
+++ script/socialpost.js
@@ -0,0 +1,10 @@
+const ul = document.querySelector("#follow .fol-link ul")
+const add = document.querySelector(".address ul")
+
+const so ='<li><a target="_blank" href="https://www.facebook.com/maishamalihablog/"><img class="icon" src="../images/icon/fb.png" alt=""></a></li><li><a target="_blank" href="https://www.instagram.com/maisha_maliha_/?hl=en"><img class="icon" src="../images/icon/insta.png" alt=""></a></li><li><a target="_blank" href="https://twitter.com/Maisha_Maliha_"><img class="icon" src="../images/icon/twt.png" alt=""></a></li><li><a target="_blank" href="https://www.youtube.com/channel/UCi4-CipsLOgdqVsmIY0lXIA"><img class="icon" src="../images/icon/you.png" alt=""></a></li><li><a target="_blank" href="https://linkedin.com/in/maishaofficially"><img class="icon" src="../images/icon/link.png" alt=""></a></li><li><a target="_blank" href="https://github.com/maisha-maliha"><img class="icon" src="../images/icon/git.png" alt=""></a></li>'
+
+
+ul.innerHTML = so
+add.innerHTML = so
+
+
